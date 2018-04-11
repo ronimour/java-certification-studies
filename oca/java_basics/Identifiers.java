01eefb4960d0a73711235403b72b0809d1b5a5c7
Ronny Moura <ronimour@gmail.com>
2018-04-10 23:17:43 -0300

    Identifiers execices and .gitignore include

---
+++ oca/java_basics/Identifiers.java
@@ -0,0 +1,15 @@
+public class Identifiers{
+
+  //int carceçeter; invalid
+  int $any; //valid
+  int any$thing; //valid
+  int _any;//valid
+  int any_;//valid
+  int any_thing;//valid
+  //int 7teen;//invalid
+  int seven7;//valid
+
+  public static void main(String[] args) {
+
+  }
+}
